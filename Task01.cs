7274c6b7097727e108c5ac3e25a030f8e9833d91
apbesshapov <90344734+apbesshapov@users.noreply.github.com>
2021-09-12 15:07:17 +0300

    Add files via upload
    HW_1 Seminars

---
+++ Task01.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Task02.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******* ******* ******* *  *  * ******* ******* ******* ****** ");
+            Console.WriteLine("*       *       *       *  *  * *     * *     * *     * *     *");
+            Console.WriteLine("*       *       *       *  *  * *     * *     * *     * *     *");
+            Console.WriteLine("*       *       *       *  *  * *     * *     * *     * *     *");
+            Console.WriteLine("******* ******* *       *  *  * ******* *     * *     * ****** ");
+            Console.WriteLine("*     * *       *       *  *  * *     * *     * *     * *     *");
+            Console.WriteLine("*     * *       *       *  *  * *     * *     * *     * *     *");
+            Console.WriteLine("*     * *       *       *  *  * *     * *     * *     * *     *");
+            Console.WriteLine("******* ******* ******* ******* *     * *     * ******* ****** ");
+        }
+    }
+}
